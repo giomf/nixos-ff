806af3d79a69ef8fb84eb2d2b0e95ee30d2d2e2d
Guillaume Fournier <me@guif.dev>
2024-11-07 08:38:33 +0100

    fix cage not starting

--- hosts/pi/divera-monitor/default.nix
+++ hosts/pi/divera-monitor/default.nix
@@ -2,13 +2,20 @@
 let
   # divera-monitor = pkgs.callPackage ./monitor.nix {};
   monitor_url = "https://app.divera247.com/monitor/1.html";
-in {
+in
+{
   age.secrets.divera-monitor-autologin.file = ../../../secrets/divera-monitor-autologin.age;
-  environment.defaultPackages = with pkgs; [ chromium wlr-randr ];
+  environment.defaultPackages = with pkgs; [
+    chromium
+    wlr-randr
+  ];
 
   services.cage = {
     enable = true;
     user = config.users.users.guif.name;
+    environment = {
+      WLR_LIBINPUT_NO_DEVICES = "1";
+    };
     program = pkgs.writeShellScript "divera-monitor" ''
       AUTO_LOGIN="$(sudo cat ${config.age.secrets.divera-monitor-autologin.path})"
       wlr-randr --output Unknown-1 --transform 90
